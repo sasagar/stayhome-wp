5751312dc35540f16cd760a499d65d974fd2b51c
SASAGAWA Kiyoshi <sasagawa@kent-and-co.com>
2021-01-01 13:09:31 +0900

    first commit.

---
+++ dist/functions.php
@@ -0,0 +1,6 @@
+<?php
+
+// theme supports
+add_theme_support( 'html5' );
+add_theme_support( 'title-tag' );
+add_theme_support( 'post-thumbnails' );
---
+++ dist/header.php
@@ -0,0 +1,7 @@
+<!doctype html>
+<html lang="<?php bloginfo("lang"); ?>">
+<head>
+<meta charset="<?php bloginfo("charset"); ?>">
+<?php wp_head(); ?>
+</head>
+<body>
