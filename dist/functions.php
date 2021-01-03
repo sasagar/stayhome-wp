498b8db4cac0aba5f26333d8127efd59475ae6e6
SASAGAWA Kiyoshi <sasagawa@kent-and-co.com>
2021-01-03 11:51:22 +0900

    add ogp support.

--- dist/functions.php
+++ dist/functions.php
@@ -35,7 +35,6 @@ add_action( 'wp_enqueue_scripts', 'sha_load_scripts' );
  * @param Object $wp_customize customizer instance.
  */
 function sha_theme_customize_register( $wp_customize ) {
-	// この中に追加していく（セクション、セッティング、コントロール）.
 	$wp_customize->add_section(
 		'sha_theme_header_section',
 		array(
@@ -68,7 +67,7 @@ function sha_theme_customize_register( $wp_customize ) {
 	);
 
 	$wp_customize->add_control(
-		new WP_Customize_Image_Control(
+		new WP_Customize_Control(
 			$wp_customize,
 			'va_alliance',
 			array(
@@ -80,6 +79,62 @@ function sha_theme_customize_register( $wp_customize ) {
 			)
 		)
 	);
+
+	$wp_customize->add_section(
+		'sha_theme_ogp_section',
+		array(
+			'title'    => 'OGP設定', // 項目名.
+			'priority' => 90, // 優先順位.
+		)
+	);
+
+	$wp_customize->add_setting( 'ogp_img' );
+
+	$wp_customize->add_control(
+		new WP_Customize_Image_Control(
+			$wp_customize,
+			'ogp_img',
+			array(
+				'label'       => 'デフォルトOGP画像', // 設定項目のタイトル.
+				'section'     => 'sha_theme_ogp_section', // 追加するセクションのID.
+				'settings'    => 'ogp_img', // 追加する設定項目のID.
+				'description' => '指定が無かったときにデフォルトで使用するOGP画像を設定してください。', // 設定項目の説明.
+			)
+		)
+	);
+
+	$wp_customize->add_setting( 'ogp_twaccount' );
+
+	$wp_customize->add_control(
+		new WP_Customize_Control(
+			$wp_customize,
+			'ogp_twaccount',
+			array(
+				'label'       => 'Twitterアカウント', // 設定項目のタイトル.
+				'section'     => 'sha_theme_ogp_section', // 追加するセクションのID.
+				'settings'    => 'ogp_twaccount', // 追加する設定項目のID.
+				'description' => 'Twitterアカウントを@付きで入力してください。', // 設定項目の説明.
+				'type'        => 'text',
+			)
+		)
+	);
+
+	$wp_customize->add_setting( 'ogp_fb' );
+
+	$wp_customize->add_control(
+		new WP_Customize_Control(
+			$wp_customize,
+			'ogp_fb',
+			array(
+				'label'       => 'FB App ID', // 設定項目のタイトル.
+				'section'     => 'sha_theme_ogp_section', // 追加するセクションのID.
+				'settings'    => 'ogp_fb', // 追加する設定項目のID.
+				'description' => 'Facebook App IDが有れば入力してください。', // 設定項目の説明.
+				'type'        => 'text',
+			)
+		)
+	);
+
 }
 add_action( 'customize_register', 'sha_theme_customize_register' );
 
@@ -89,6 +144,78 @@ add_action( 'customize_register', 'sha_theme_customize_register' );
 function sha_get_the_logo_img_url() {
 	return esc_url( get_theme_mod( 'logo_img' ) );
 }
+
+/**
+ * OGP output from theme customizer.
+ */
+function sha_ogp() {
+	$og_img      = get_theme_mod( 'ogp_img', false );
+	$og_twaccont = get_theme_mod( 'ogp_twaccount', false );
+	$og_fb       = get_theme_mod( 'ogp_fb', false );
+
+	if ( $og_img && $og_twaccont ) {
+
+		if ( is_front_page() || is_home() || is_singular() ) {
+			global $post;
+			$ogp_title = '';
+			$ogp_descr = '';
+			$ogp_url   = '';
+			$ogp_img   = '';
+			$insert    = '';
+
+			if ( is_singular() ) { // 記事＆固定ページ.
+				setup_postdata( $post );
+				$ogp_title = $post->post_title;
+				$ogp_descr = mb_substr( get_the_excerpt(), 0, 100 );
+				$ogp_url   = get_permalink();
+				wp_reset_postdata();
+			} elseif ( is_front_page() || is_home() ) { // トップページ.
+				$ogp_title = get_bloginfo( 'name' );
+				$ogp_descr = get_bloginfo( 'description' );
+				$ogp_url   = home_url();
+			}
+
+			// og:type.
+			$ogp_type = ( is_front_page() || is_home() ) ? 'website' : 'article';
+
+			// og:image.
+			if ( is_singular() && has_post_thumbnail() ) {
+				$ps_thumb = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' );
+				$ogp_img  = $ps_thumb[0];
+			} else {
+				$ogp_img = $og_img;
+			}
+
+			// 出力するOGPタグをまとめる.
+			$insert .= '<meta property="og:title" content="' . esc_attr( $ogp_title ) . '">' . "\n";
+			$insert .= '<meta property="og:description" content="' . esc_attr( $ogp_descr ) . '">' . "\n";
+			$insert .= '<meta property="og:type" content="' . $ogp_type . '">' . "\n";
+			$insert .= '<meta property="og:url" content="' . esc_url( $ogp_url ) . '">' . "\n";
+			$insert .= '<meta property="og:image" content="' . esc_url( $ogp_img ) . '">' . "\n";
+			$insert .= '<meta property="og:site_name" content="' . esc_attr( get_bloginfo( 'name' ) ) . '">' . "\n";
+			$insert .= '<meta name="twitter:card" content="summary_large_image">' . "\n";
+			$insert .= '<meta name="twitter:site" content="' . $og_twaccont . '">' . "\n";
+			$insert .= '<meta property="og:locale" content="ja_JP">' . "\n";
+
+			if ( $og_fb ) {
+				$insert .= '<meta property="fb:app_id" content="' . $og_fb . '">' . "\n";
+			}
+
+			echo wp_kses(
+				$insert,
+				array(
+					'meta' => array(
+						'property' => array(),
+						'name'     => array(),
+						'content'  => array(),
+					),
+				)
+			);
+		}
+	}
+}
+
+add_action( 'wp_head', 'sha_ogp' ); // headにOGPを出力.
 
 /**
  * Add nav menu.
